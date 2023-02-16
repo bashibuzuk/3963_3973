a88719645b49223f0ac9586ca563be1aaae2a272
Roma <7irs@bk.ru>
2023-02-16 23:05:13 +0300

    домашние задания часть 2

---
+++ HomeWork02Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+System.Random numSint = new System.Random();
+
+int rndNumber = numSint.Next(10,999999999);             // синтез случайного числа
+Console.WriteLine($"Случайное число {rndNumber}");
+if (rndNumber < 100)                                    //  проверка случайного числа на двузначность
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else                                                    // запуск цикла, который будет работать до тех пор, пока
+{                                                       //число не станет трехначным
+    while(rndNumber > 1000)
+    {
+        rndNumber = rndNumber/10;
+    }
+Console.WriteLine($"Третье число равно - {rndNumber%10}");
+}
---
+++ HomeWork02Task15/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите программу, которая принимает на вход цифру, 
+//,  обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите любое число");
+int Number1 = int.Parse(Console.ReadLine()??"0");
+int Result = Number1%7;
+if (Result == 0 || Result == 6)
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Будень!!!");
+}
